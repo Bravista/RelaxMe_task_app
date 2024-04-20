d8d00f4e5a88286298ab0cd0fc92cddcfe3db9f4
jchillah <jchillah@gmail.com>
2024-04-20 02:04:15 +0200

    s_5_1_1_2 solved

--- lib/domain/styles.dart
+++ lib/domain/styles.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class AppStyles {
-  static const double defaultPadding = 8.0;
-  static const double elementSpacing = 20.0;
+class Styles {
+  static const double defaultPadding = 8.0; // padding for Container with Image
+  static const double elementSpacing = 20.0;  // spacing for SizedBoxes
 
-  static final TextStyle titleStyle = GoogleFonts.oswald(
-    fontSize: 16,
-    fontWeight: FontWeight.bold,
+  static final TextStyle titleStyle = GoogleFonts.oswald( //font i want to use for titles like Name, Weight, Size, favorite food
+    fontSize: 16, // fontSize for titles
+    fontWeight: FontWeight.bold,  // fontweight for titles
   );
 
-  static const TextStyle defaultTextStyle = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.normal,
+  static const TextStyle defaultTextStyle = TextStyle(  // TextStyle for descriptions of titles
+    fontSize: 14, // fontsize for descriptions
+    fontWeight: FontWeight.normal,  //fontweight for descriptions
   );
 }
--- lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2.dart
@@ -10,37 +10,37 @@ class S5112 extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Container(
-            padding: const EdgeInsets.all(AppStyles.defaultPadding),
-            child: Image.network("https://image.jimcdn.com/app/cms/image/transf/dimension=310x10000:format=jpg/path/s6daccef63eabd0db/image/i513835b33600eb00/version/1499505232/image.jpg"),
-            height: 400.0,  
+            padding: const EdgeInsets.all(Styles.defaultPadding),
+            height: 400.0,
+            child: Image.network("https://image.jimcdn.com/app/cms/image/transf/dimension=310x10000:format=jpg/path/s6daccef63eabd0db/image/i513835b33600eb00/version/1499505232/image.jpg"),  
           ),
           const SizedBox(
             height: 8.0,
           ),
           Text(
             "Name",
-            style: AppStyles.titleStyle,
+            style: Styles.titleStyle,
           ),
           const Text("Diana das Lama"),
           const SizedBox(
-            height: AppStyles.elementSpacing,
+            height: Styles.elementSpacing,
           ),
           Text("Gewicht",
-            style: AppStyles.titleStyle,
+            style: Styles.titleStyle,
           ),
           const Text("120 kg"),
           const SizedBox(
-            height: AppStyles.elementSpacing,
+            height: Styles.elementSpacing,
           ),
           Text("Größe",
-            style: AppStyles.titleStyle,
+            style: Styles.titleStyle,
           ),
           const Text("1,80 m"),
           const SizedBox(
-            height: AppStyles.elementSpacing,
+            height: Styles.elementSpacing,
           ),
           Text("Lieblingsessen",
-            style: AppStyles.titleStyle,
+            style: Styles.titleStyle,
           ),
           const Text("Gras")
         ],
